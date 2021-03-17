c1d55b4926fa4b96f1de9b162e27286c23e320a6
vbasoon <vbasoon@gmail.com>
2021-03-17 14:40:50 +0200

    SPA

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <router-link class="navlink" to="/">Home</router-link>
+        <router-link class="navlink" to="/about">About</router-link>
+        <slot>Slot</slot>
+        <slot name='H3'></slot>
+        <h1>I am footer</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MainLayout',
+
+}
+</script>
+
+<style scoped>
+.navlink {
+    margin-left: 10px;
+    text-decoration: none;
+    font-size: 20px;
+}
+
+.navlink:hover {
+    
+    text-decoration: underline;
+    
+}
+
+
+</style>
