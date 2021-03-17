c4ba24878bac19fddea4cad25b5d384b8ff3b00d
vbasoon <vbasoon@gmail.com>
2021-03-17 12:31:04 +0200

    @

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,18 @@
 import VueRouter from 'vue-router'
-import Home from '../pages/Home/Home'
-import About from '../pages/About/About'
+import Home from '@/pages/Home/Home'
+import About from '@/pages/About/About'
 
 export default new VueRouter({
     mode: 'history',
 routes: [
     {
         path: '',
+        name: 'Home',
         component: Home
     },
     {
         path: '/about',
+        name: 'About',
         component: About
     },
     ]
