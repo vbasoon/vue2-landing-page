e4d901142cb4134efee58c5eacb468936a59d959
vbasoon <vbasoon@gmail.com>
2021-03-17 12:17:40 +0200

    router-view delete nasty component

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Home from '../pages/Home/Home'
 import About from '../pages/About/About'
 
 export default new VueRouter({
+    mode: 'history',
 routes: [
     {
         path: '',
@@ -12,5 +13,6 @@ routes: [
         path: '/about',
         component: About
     },
-]
+    ]
+    
 })
