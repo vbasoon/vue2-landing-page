07ebc5c8a210be47b4433936b8acddf7cb24b4c5
vbasoon <vbasoon@gmail.com>
2021-03-17 12:00:03 +0200

    routes

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
 import router from './router'
 
 Vue.config.productionTip = false
@@ -8,4 +9,5 @@ Vue.use(VueRouter)
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,16 @@
-import Vuerouter from 'vue-router'
+import VueRouter from 'vue-router'
+import Home from '../pages/Home/Home'
+import About from '../pages/About/About'
 
 export default new VueRouter({
-
+routes: [
+    {
+        path: '',
+        component: Home
+    },
+    {
+        path: '/about',
+        component: About
+    },
+]
 })
