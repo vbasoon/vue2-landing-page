c606b5727c90685280b9f38b58400d1cd247d6c0
Басун Володимир <vbasoon@gmail.com>
2021-03-19 15:08:16 +0200

    array cards

--- src/pages/Advertising/Advertising.vue
+++ src/pages/Advertising/Advertising.vue
@@ -8,13 +8,27 @@
 </template>
 
 <script>
-import Card from  '@/components/Card/Card.vue'
 import MainLayout from '@/layouts/MainLayout'  
+import Card from  '@/components/Card/Card.vue'
+
 export default {
+  name: "Advetrising",
+  data() {
+    return {
+      cards: [
+        {id: 1, title: "1blablalala"},
+        {id: 2, title: "2blablalala"},
+        {id: 3, title: "3blablalala"},
+        {id: 4, title: "4blablalala"},
+        {id: 5, title: "5blablalala"},
+        
+      ]
+    }
+  },
   components: {
   MainLayout,
   Card
-}
+  }
 }
 </script>
 
