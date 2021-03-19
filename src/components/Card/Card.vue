f5e9899199158eb4cb2e1c2028b7fab23d35bed0
Басун Володимир <vbasoon@gmail.com>
2021-03-19 15:07:59 +0200

    array cards

--- src/components/Card/Card.vue
+++ src/components/Card/Card.vue
@@ -8,18 +8,7 @@
 <script>
 export default {
   name: "Card",
-  data() {
-    return {
-      cards: [
-        {id: 1, title: "1blablalala"},
-        {id: 2, title: "2blablalala"},
-        {id: 3, title: "3blablalala"},
-        {id: 4, title: "4blablalala"},
-        {id: 5, title: "5blablalala"},
-        
-      ]
-    }
-  }
+  
 }
 </script>
 
